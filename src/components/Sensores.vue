d2560f81ffc59e7361c4e24b4acdcd3f2a0f496b
BigThor4673 <victor.matta.p@gmail.com>
2024-07-14 03:54:19 -0400

    REFACTOR | sensores
    Se reestructuran sensores y db firestore para dejar preparado en caso de mas de una maceta.
    Se leen los datos de la plata y de la subcoleccion sensores, en tiempo real.

--- src/components/Sensores.vue
+++ src/components/Sensores.vue
@@ -5,54 +5,128 @@
     </div>
     <div class="col-md-4">
       <h4>Humedad Tierra</h4>
-      <strong>{{ sensorHumedadTierra }}</strong>
+      <strong>{{ sensorHumedadSuelo }}</strong>
     </div>
     <div class="col-md-4">
       <h4>Humedad Ambiente</h4>
       <strong>{{ sensorHumedadAmbiente }}</strong>
     </div>
     <div class="col-md-4">
+      <h4>Temperatura Ambiente</h4>
+      <strong>{{ sensorTemperaturaAmbiente }}</strong>
+    </div>
+    <div class="col-md-4">
       <h4>Luz</h4>
       <strong>{{ sensorLuz }}</strong>
+    </div>
+    <div class="col-md-4">
+      <h4>Luz Update</h4>
+      <strong>{{ sensorLuzFecha }}</strong>
+    </div>
+    <div class="col-md-4">
+      <h4>Nombre</h4>
+      <strong>{{ plantaData.nombre }}</strong>
+    </div>
+    <div class="col-md-4">
+      <h4>Banco</h4>
+      <strong>{{ plantaData.banco }}</strong>
+    </div>
+    <div class="col-md-4">
+      <h4>Tipo</h4>
+      <strong>{{ plantaData.tipo }}</strong>
+    </div>
+    <div class="col-md-4">
+      <h4>Tipo Planta</h4>
+      <strong>{{ plantaData.tipo_planta }}</strong>
+    </div>
+    <div class="col-md-4">
+      <h4>Fecha Germinación</h4>
+      <strong>{{ plantaData.fecha_germinacion ? plantaData.fecha_germinacion.toDate().toLocaleDateString() : null }}</strong>
     </div>
   </div>
 </template>
 
 <script>
-import SensoresService from "../services/SensoresService";
+import { db } from "../firebase";
 
 export default {
   name: "sensores",
   data() {
     return {
-      sensorHumedadTierra: null,
+      unsubscribeDocument: null,
+      unsubscribeSensores: null,
+      sensorHumedadSuelo: null,
+      sensorTemperaturaAmbiente: null,
       sensorHumedadAmbiente: null,
       sensorLuz: null,
-      unsubscribe: null
+      sensorLuzFecha: null,
+      plantaData: {
+        nombre: null,
+        tipo: null, 
+        tipo_planta: null,
+        banco: null,
+        fecha_germinacion: null
+      },
     };
   },
   methods: {
-    onDataChange(items) {
-      items.forEach((item) => {
-        //FIXME: Corregir la llamada a la colección para que también lea los metadatos raza, banco y germinacion.
-        let data = item.data();
-        if(item.id == "humedad-tierra"){
-          this.sensorHumedadTierra = data.valor;
-        }
-        else if(item.id == "humedad-ambiente"){
-          this.sensorHumedadAmbiente = data.valor;
-        }
-        else if(item.id == "luz"){
-          this.sensorLuz = data.valor;
-        }
-      });
-    },
+    fetchData() {
+      try {
+        const docRef = db.collection('inv-data').doc('Inv-001').collection('maceteros').doc('ma-z01');
+        // Verificación del documento principal
+        this.unsubscribeDocument = docRef.onSnapshot(docSnapshot => {
+          if (docSnapshot.exists) {
+            console.log("Documento encontrado:", docSnapshot.data());
+            this.plantaData = docSnapshot.data();
+          } else {
+            console.log("El documento no existe");
+          }
+        }, error => {
+          console.error("Error al obtener el documento:", error);
+        });
+
+        // Verificación de la colección "sensores"
+        this.unsubscribeSensores = docRef.collection('sensores').onSnapshot(sensoresSnapshot => {
+          sensoresSnapshot.forEach(doc => {
+            let itemData = doc.data();
+            if(doc.id == "humedad-ambiente"){
+              this.sensorHumedadAmbiente = itemData.valor;
+            }
+            else if(doc.id == "humedad-suelo"){
+              this.sensorHumedadSuelo = itemData.valor;
+            }
+            else if(doc.id == "temperatura-ambiente"){
+              this.sensorTemperaturaAmbiente = itemData.valor;
+            }
+            else if(doc.id == "luz"){
+              this.sensorLuz = itemData.valor;
+              let luzFecha = itemData.fecha_update.toDate().toLocaleDateString()
+              let luzHora = itemData.fecha_update.toDate().toLocaleTimeString()
+              this.sensorLuzFecha = luzFecha+" "+luzHora;
+            }
+          });
+        }, error => {
+          console.error("Error al obtener la colección de sensores:", error);
+        });
+
+      } catch (error) {
+        console.error("Error en fetchData:", error);
+      }
+    }
+  },
+  created() {
+    this.fetchData();
   },
   mounted() {
-    this.unsubscribe = SensoresService.getAll().onSnapshot(this.onDataChange);
+
   },
   beforeUnmount() {
-    this.unsubscribe();
+    if (this.unsubscribeDocument) {
+      this.unsubscribeDocument();
+    }
+    if (this.unsubscribeSensores) {
+      this.unsubscribeSensores();
+    }
   }
 };
 </script>
