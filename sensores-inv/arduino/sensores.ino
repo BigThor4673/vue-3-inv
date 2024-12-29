25fd9b81b0e11b6c5089b408990d9b5da34e4c01
BigThor4673 <victor.matta.p@gmail.com>
2024-12-29 03:03:41 -0300

    Se estructura el mensaje en json a imprimir cada 10 segundos por parte de los sensores en arduino

--- sensores-inv/arduino/sensores.ino
+++ sensores-inv/arduino/sensores.ino
@@ -5,9 +5,12 @@ dht DHT;
 
 int fotoPin = A5;
 int fotoValor;
+int luzValor;
 
 int rainPin = A0;
 int thresholdValue = 800;
+
+int sueloValue;
 
 void setup()
 {
@@ -19,37 +22,45 @@ void setup()
 void loop()
 {
     //Genera un array de dos posiciones para almacenar el nombre del sensor en la primera posición, y el valor del sensor en la segunda.
-    /*String sensorData[2];
-    sensorData[0] = "DHT11";
-    sensorData[1] = "Temperature = " + String(DHT.temperature) + " Humidity = " + String(DHT.humidity);
-    Serial.println(sensorData[0] + " " + sensorData[1]);*/
+    // String sensorData[2];
+    // sensorData[0] = "DHT11";
+    // sensorData[1] = "Temperature = " + String(DHT.temperature) + " Humidity = " + String(DHT.humidity);
+    // Serial.println(sensorData[0] + " " + sensorData[1]);
 
     /***** DHT11: Lee la temperatura y la humedad ambiental *****/
     int chk = DHT.read11(DHT11_PIN);
-    Serial.print("Temperature = ");
-    Serial.println(DHT.temperature);
-    Serial.print("Humidity = ");
-    Serial.println(DHT.humidity);
-    delay(2000);
+    // Serial.print("{'temperatura-ambiente': "+DHT.temperature+", 'humedad-ambiente': "+DHT.humidity+"}");
+    // delay(2000);
     /***** DHT11 *****/
 
     /***** FOTORESISTENCIA: Detecta si la luz está encendida o apagada *****/
     //Arroja 900 con luz apagada.
     //Si el valor es menor a 700, esta encendida.
     fotoValor = analogRead(fotoPin);
-    Serial.println(fotoValor);
-    delay(2000); 
+    
+    if(fotoValor < 700){
+        luzValor = 1;
+    }
+    else{
+        luzValor = 0;
+    }
+    // Serial.println("{'luz': luzValor}");
+    // Serial.println(fotoValor);
+    // delay(2000); 
     /***** FOTORESISTENCIA *****/
 
     /***** YL69: Lee la humedad de la tierra *****/
-    int sensorValue = analogRead(rainPin);
-    Serial.print(sensorValue);
-    if(sensorValue < thresholdValue){
-        Serial.println(" - Doesn't need watering");
-    }
-    else {
-        Serial.println(" - Time to water your plant");
-    }
-    delay(2000);
+    sueloValue = analogRead(rainPin);
+    // Serial.println("{ 'humedad-suelo': sueloValue }");
+    // if(sueloValue < thresholdValue){
+    // }
+    // else {
+    //     Serial.println(" - Time to water your plant");
+    // }
+    // delay(2000);
     /***** YL69 *****/
+
+    Serial.print("{'temperatura-ambiente': "+DHT.temperature+", 'humedad-ambiente': "+DHT.humidity+", 'luz': luzValor+", 'humedad-suelo': sueloValue+"}");
+    delay(5000);
 }
+
