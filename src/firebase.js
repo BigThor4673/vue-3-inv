3f93a8d0ba461b43cd7d3dffebb3656c0a1215c2
Victor Matta <victor.matta.p@gmail.com>
2026-08-08 00:10:22 -0400

    FIX .env variables y ejecucion npm

--- src/firebase.js
+++ src/firebase.js
@@ -22,13 +22,13 @@ import 'firebase/firestore';
 
 // Configuración de Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyD0XCA5pL5swZE38N_XHT_JJT1Uc_OeIyg",
-  authDomain: "fir-inv-72799.firebaseapp.com",
-  databaseURL: "https://fir-inv-72799-default-rtdb.firebaseio.com",
-  projectId: "fir-inv-72799",
-  storageBucket: "fir-inv-72799.appspot.com",
-  messagingSenderId: "213323648848",
-  appId: "1:213323648848:web:5318a93dc667f0c10ba655",
+  apiKey: process.env.VUE_APP_APIKEY,
+  authDomain: process.env.VUE_APP_AUTHDOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASEURL,
+  projectId: process.env.VUE_APP_PROJECTID,
+  storageBucket: process.env.VUE_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGINGSENDERID,
+  appId: process.env.VUE_APP_APPID,
 };
 
 // Inicializa Firebase
